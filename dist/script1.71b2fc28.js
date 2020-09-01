0f2b870ea8e3053fd807060cb8133747fa84da8f
Anuradha <anusreeganesh@gmail.com>
2020-09-01 02:39:52 -0500

    Resolved gitignore issues

--- dist/script1.71b2fc28.js
+++ dist/script1.71b2fc28.js
@@ -603,7 +603,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60314" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58430" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
